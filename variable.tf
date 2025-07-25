cdfcc00aeb6b0318197dc09dbfd6eed797958a1e
SONALSUBODH <ms.sonalkulkarni@gmail.com>
2025-07-25 21:50:05 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,35 @@
+variable "ami_id" {
+  type = string
+
+}
+
+variable "instance_type" {
+  type = string
+
+}
+
+variable "instance_tags" {
+  type = map
+
+}
+
+variable "sg_name" {
+  type = string
+
+ }
+
+variable "sg_description" {
+  type = string
+
+}
+
+variable "vpc_id" {
+  type = string
+
+}
+
+variable "sg_tags" {
+  type = map
+
+}
+  
