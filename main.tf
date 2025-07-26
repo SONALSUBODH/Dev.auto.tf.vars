1a48f98a576d7c887672d00a04d8f1ce76327f74
SONALSUBODH <ms.sonalkulkarni@gmail.com>
2025-07-26 20:24:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 resource "aws_instance" "demo" {
-   ami              = var.ami_id
-   instance_type    = var.instance_type
-   security_groups  = [aws_security_group.tf_sg.name]
-   tags             = var.instance_tags
+  ami             = var.ami_id
+  instance_type   = var.instance_type
+  security_groups = [aws_security_group.tf_sg.name]
+  tags            = var.instance_tags
 
 }
 resource "aws_security_group" "tf_sg" {
-  name        = "tf_sg"
+  name        = "tf_sg8"
   description = "Allow HTTPS to web server"
   vpc_id      = "vpc-05bdcc8880aab85ab"
 
@@ -15,14 +15,14 @@ resource "aws_security_group" "tf_sg" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    
+
   }
- ingress {
+  ingress {
     description = "TLS from VPC"
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
-    
+
   }
   egress {
     from_port   = 0
@@ -35,3 +35,4 @@ resource "aws_security_group" "tf_sg" {
 data "aws_vpc" "default" {
   default = true
 }
+
